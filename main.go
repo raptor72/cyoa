913268daded213871b18bc4b1ecb33789e58a302
raptor72 <mctaras51@yandex.ru>
2021-12-25 17:59:05 +0400

    add css

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ const tmpl = `
         <title>Choose Tour Own Adventure</title>
     </head>
     <body>
+        <section class="page">
         <h1>{{.Title}}</h1>
         {{range .Story}}<p>{{ . }}</p>{{else}}<p><strong>no rows</strong></p>{{end}}
         <ul>
@@ -39,6 +40,47 @@ const tmpl = `
         </li>
         {{end}}
         </ul>
+        </section>
+    <style>
+        body {
+           font-family: helvetica, arial;
+        }
+        h1 {
+            text-align:center;
+            position:relative;
+        }
+        .page {
+            width: 80%;
+            max-width: 500px;
+            margin: auto;
+            margin-top: 40px;
+            margin-bottom: 40px;
+            padding: 80px;
+            background: #FFFCF6;
+            border: 1px solid #eee;
+            box-shadow: 0 10px 6 px -6px #777;
+        }
+        ul {
+            border-top: 1px dotted #ccc
+            padding: 10px 0 0 0;
+            -webkit-padding-start: 0;
+        }
+        li {
+            padding-top: 10px;
+        }
+        a,
+        a:visited {
+            text-decoration: none:
+            color: #6295b5;
+        }
+        a:active,
+        a:hover {
+            color: #7792a2;
+        }
+        p {
+            text-indent: 1em;
+        }
+    </style>
     </body>
 </html>`
 
